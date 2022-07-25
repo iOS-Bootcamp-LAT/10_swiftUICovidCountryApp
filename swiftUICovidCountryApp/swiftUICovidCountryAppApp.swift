33b455db1ee8ad581959fce61334bd705c099c3c
David Granado J <david.granado@interfase.com.bo>
2022-07-25 08:55:04 -0400

    Initial Commit

---
+++ swiftUICovidCountryApp/swiftUICovidCountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUICovidCountryAppApp.swift
+//  swiftUICovidCountryApp
+//
+//  Created by David Granado Jordan on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUICovidCountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
